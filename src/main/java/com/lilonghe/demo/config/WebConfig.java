f91771da121154f4f7f9cb15db6ecd08162d8bb6
lilonghe <lilonghe@outlook.com>
2024-08-08 11:12:41 +0800

    feat: add cors config

--- src/main/java/com/lilonghe/demo/config/WebConfig.java
+++ src/main/java/com/lilonghe/demo/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.lilonghe.demo.config;
 
 import com.lilonghe.demo.interceptor.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +18,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor).addPathPatterns("/*");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
